e00c6a5cf4231d0ac7451e900d1a34c5efd631d2
Mostafa Rahim Taha <61434616+mostafarahimtaha@users.noreply.github.com>
2026-09-26 21:08:49 +0300

    Update simplyCountdown.js

--- countdown/simplyCountdown.js
+++ countdown/simplyCountdown.js
@@ -34,6 +34,7 @@ const m = {
   hours: 14,
   minutes: 30,
   seconds: 0,
+  enableUtc: false,
   words: {
     days: { lambda: (n, e) => e > 1 ? n + "s" : n, root: "day" },
     hours: { lambda: (n, e) => e > 1 ? n + "s" : n, root: "hour" },
